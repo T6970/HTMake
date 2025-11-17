aad030a4ee117d322e94cc0f2501a13d37107869
T6970 <tenaldkyf315@gmail.com>
2025-11-17 21:06:02 +0800

    Add content to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+import { tagList } from "./html-tags.js";
+import { setCard } from "./set-card.js";
+const toolbar = document.getElementById("toolbar");
 
+toolbar.style.overflow = "auto"
+toolbar.style.display  = "flex"
+toolbar.style.flexDirection = "column"
+setCard(toolbar, 20, 20, 20, 20)
+
+tagList.tags.forEach((tag) => {
+  const eachButton = document.createElement("button");
+  eachButton.name = tag;
+  eachButton.addEventListener("click", () => {});
+  eachButton.className = "card";
+  eachButton.innerText = tag;
+
+  toolbar.appendChild(eachButton);
+})
