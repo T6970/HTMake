430cbd62ca64162437656a054dbc3986a7755a20
T6970 <tenaldkyf315@gmail.com>
2025-11-17 20:48:22 +0800

    These is list of HTML tags

---
+++ js/html-tags.js
@@ -0,0 +1,21 @@
+export const tagList = {
+  tags: [
+    "meta", "title", "link", "script", "noscript", "style",
+    "div", "span", "p", "a", "img",
+    "h1", "h2", "h3", "h4", "h5", "h6",
+    "ul", "ol", "li", "header", "footer", "main", "nav", "section", "article",
+    "form", "input", "label", "button", "select", "option", "textarea",
+  
+    "video", "audio", "source", "picture", "canvas", "svg",
+    "table", "tr", "td", "th", "thead", "tbody", "tfoot",
+    "aside", "figure", "figcaption", "details", "summary",
+    "progress", "meter", "dialog",
+  
+    "iframe", "blockquote", "cite", "code", "pre", "kbd", "samp", "var",
+    "mark", "time", "small", "abbr", "dfn", "sup", "sub", "hr",
+  ],
+  attributes: [
+    "class",
+    "id"
+  ]
+}
