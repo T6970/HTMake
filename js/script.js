4472b0563141fc9c39352c342ac106eef11934ea
T6970 <tenaldkyf315@gmail.com>
2025-12-18 12:44:31 +0800

    Now iframe has styles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-import { newCard } from "./set-card.js";
+import { newCard } from "./card.js";
 
 const pframe = document.getElementById("preview")
 pframe.addEventListener("load", () => {
   pframe.srcdoc = '<!DOCTYPE html><html><head></head><body>Write here</body></html>'
   const preview = pframe.contentDocument || pframe.contentWindow.document
   preview.designMode = "on"
-  
+  const stylesheet = preview.createElement("link")
+  stylesheet.rel = "stylesheet"
+  stylesheet.href = "css/preview.css"
+  preview.head.appendChild(stylesheet)
+
   const test = newCard(100, 100, 200, 300, preview)
+  console.log(test)
 })
-
-
